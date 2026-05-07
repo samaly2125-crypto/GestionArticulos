79ea76a20554135a07458b9238b32a2b660ec127
samaly2125-crypto <samaly.2125@gmail.com>
2026-05-06 19:31:08 -0500

    p8

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("mysql-marcela.alwaysdata.net", "marcela", "mazo123.", "marcela_gestion_articulos");
+$conexion = new mysqli("mysql-marcela.alwaysdata.net", "marcela", "lina123.", "marcela_gestion_articulos");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
